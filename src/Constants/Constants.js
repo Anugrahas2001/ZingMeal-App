885f198651345ae5658d31ce4e8d98d060018132
Anugrahas2001 <anugrahas2001@gmail.com>
2024-09-02 18:15:25 +0530

    axios file added

--- src/Constants/Constants.js
+++
@@ -1,9 +0,0 @@
-export const baseUrl="https://zingmeal.onrender.com"
-export const UPDATE_CART_DECREMENT=-1
-export const UPDATE_CART_INCREMENT=1
-export const UPDATE_CART_REMOVE=0
-export const PREPARING="preparing"
-export const PLACED="placed"
-export const PICKUP="pickup"
-export const DISPATCHED="Dispatched"
-export const DELIVERED="Delivered"
