21280352b3092c18b18fab82a7bea1f9a305b645
Anugrahas2001 <anugrahas2001@gmail.com>
2024-09-07 20:42:54 +0530

    added some modification on footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,27 +1,54 @@
-import React from 'react';
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+} from "@fortawesome/free-solid-svg-icons";
+
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white pt-7 pb-5 mt-10 text-base sm:text-sm xs:text-xs">
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap justify-between items-start space-y-8 lg:space-y-0 md:space-y-0">
-          
           <div className="w-full lg:w-1/4 md:w-1/2 sm:w-full px-4">
             <h4 className="text-xl font-semibold mb-4 xs:mb-1">About Us</h4>
             <p className="text-gray-400">
-              We bring you the best food from the top restaurants. Freshly prepared, delivered right to your door.
+              We bring you the best food from the top restaurants. Freshly
+              prepared, delivered right to your door.
             </p>
           </div>
 
           <div className="w-full lg:w-1/4 md:w-1/2 sm:w-full px-4">
             <h4 className="text-xl font-semibold mb-4 xs:mb-2">Quick Links</h4>
             <ul className="text-gray-400 space-y-2">
-              <li><a href="" className="hover:underline">Restaurants</a></li>
-              <li><a href="" className="hover:underline">About Us</a></li>
-              <li><a href="" className="hover:underline">Contact Us</a></li>
-              <li><a href="" className="hover:underline">Terms of Service</a></li>
-              <li><a href="" className="hover:underline">Privacy Policy</a></li>
+              <li>
+                <a href="" className="hover:underline">
+                  Restaurants
+                </a>
+              </li>
+              <li>
+                <a href="" className="hover:underline">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a href="" className="hover:underline">
+                  Contact Us
+                </a>
+              </li>
+              <li>
+                <a href="" className="hover:underline">
+                  Terms of Service
+                </a>
+              </li>
+              <li>
+                <a href="" className="hover:underline">
+                  Privacy Policy
+                </a>
+              </li>
             </ul>
           </div>
 
@@ -35,16 +62,28 @@ const Footer = () => {
         <div className="border-t border-gray-600 pt-6 mt-6">
           <div className="flex flex-wrap justify-between items-center">
             <div className="flex space-x-6 mb-4 lg:mb-0 md:mb-0 sm:mb-0">
-              <a href="https://facebook.com" className="text-gray-400 hover:text-white">
+              <a
+                href="https://facebook.com"
+                className="text-gray-400 hover:text-white"
+              >
                 <FaFacebookF />
               </a>
-              <a href="https://twitter.com" className="text-gray-400 hover:text-white">
+              <a
+                href="https://twitter.com"
+                className="text-gray-400 hover:text-white"
+              >
                 <FaTwitter />
               </a>
-              <a href="https://instagram.com" className="text-gray-400 hover:text-white">
+              <a
+                href="https://instagram.com"
+                className="text-gray-400 hover:text-white"
+              >
                 <FaInstagram />
               </a>
-              <a href="https://linkedin.com" className="text-gray-400 hover:text-white">
+              <a
+                href="https://linkedin.com"
+                className="text-gray-400 hover:text-white"
+              >
                 <FaLinkedinIn />
               </a>
             </div>
@@ -60,4 +99,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
